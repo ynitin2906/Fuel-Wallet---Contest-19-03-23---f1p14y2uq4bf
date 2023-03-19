a36dd111bac674ff88b608c3ebd67a5ed6ca3ade
ynitin2906 <ynitin2906@gmail.com>
2023-03-19 13:33:22 +0530

    update

--- src/reducers.js
+++ src/reducers.js
@@ -1,5 +1,22 @@
 import { v4 as uuidv4 } from "uuid";
 
-const reducer = (state, action) => {};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "add":
+      const newUser = {
+        id: uuidv4(),
+        name: action.name,
+      };
+      action.setBalance(action.balance + 10);
+      return [...state, newUser];
+    case "fuel":
+      if (action.balance > 50) {
+        action.setBalance(action.balance - 50);
+      }
+      return state;
+    default:
+      return state;
+  }
+};
 
 export default reducer;
